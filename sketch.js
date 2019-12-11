70480a3bc1e5af092abd46c21c635e323744f0ab
Anton Laurell <antonlaurell@hotmail.com>
2019-12-11 15:45:02 +0100

    m

--- sketch.js
+++ sketch.js
@@ -267,7 +267,7 @@ function draw() {
 
         // display score
         textSize(w * 0.4);
-        text(snake.body.length - 1, w * 0.5, h * 0.5)
+        text(snake.body.length - 1, w * 0.5, h * 0.6)
 
 
         if (snake.did_eat(food)) {
